75a05447ec0abf6628363c71826333d94ee5c8c6
Elian Huanca <huancacori@gmail.com>
2023-05-11 14:24:40 -0400

    Url De Laravel

--- src/const/const.js
+++ src/const/const.js
@@ -1,1 +1,2 @@
-export const baseUrl = "http://localhost/photo-market-laravel/public/api";
+//export const baseUrl = "http://localhost/photo-market-laravel/public/api";
+export const baseUrl = "https://photo-market-laravel-production.up.railway.app/api";
